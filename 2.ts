8bfc1d3fa2e4ffdf833627f6c8bb30f6aa8bf3c6
Sayyam_Shahid <sayyamshahid143@gmail.com>
2023-11-05 01:50:11 -0700

    My commit

--- 2.ts
+++ 2.ts
@@ -5,5 +5,6 @@ const variable2 : string = "Sami";
 console.log("Lowercase: " + variable2.toLowerCase());
 console.log("Uppercase:" + variable2.toUpperCase());
 const words = variable2.split(" ");
+
 const titleCaseName = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
 console.log("Title Case: " + titleCaseName);
