0f92c9ffe8b2ec14472d82976c120a4338ae1c17
Sayyam_Shahid <sayyamshahid143@gmail.com>
2023-11-05 01:49:23 -0700

    second excercise

--- 2.js
+++ 2.js
@@ -1,9 +1,7 @@
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-const variable2 : string = "Sami";
-
+var variable2 = "Sami";
 console.log("Lowercase: " + variable2.toLowerCase());
 console.log("Uppercase:" + variable2.toUpperCase());
-const words = variable2.split(" ");
-const titleCaseName = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+var words = variable2.split(" ");
+var titleCaseName = words.map(function (word) { return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); }).join(" ");
 console.log("Title Case: " + titleCaseName);
