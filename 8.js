30d5a4cd34d59b7b5053d664bdf2e7fec0d6c5f8
Sayyam_Shahid <sayyamshahid143@gmail.com>
2023-11-05 23:57:35 -0800

    execise  is completed

---
+++ 8.js
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 45;
+var message = "My Favorite Number is : ";
+console.log("".concat(message, " ").concat(favoriteNumber, " "));
