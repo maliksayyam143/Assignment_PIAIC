db91a15d931a1a100f99ae1f8a07f43401df01a1
Sayyam_Shahid <sayyamshahid143@gmail.com>
2023-11-05 22:14:01 -0800

    execise completed

---
+++ 6.js
@@ -0,0 +1,21 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addition 
+var num = 5;
+var Num = 3;
+var Result = num + Num;
+console.log("".concat(num, " + ").concat(Num, " = ").concat(Result));
+//Subtraction 
+var num1 = 20;
+var num2 = 12;
+var result = num1 - num2;
+console.log("".concat(num1, " - ").concat(num2, " = ").concat(result));
+//Multiplication
+var Num1 = 2;
+var Num2 = 4;
+var RESULT = Num1 * Num2;
+console.log("".concat(Num1, " * ").concat(Num2, " = ").concat(RESULT));
+// Division 
+var number1 = 40;
+var number2 = 5;
+var REsult = number1 / number2;
+console.log("".concat(number1, " / ").concat(number2, " = ").concat(REsult));
---
+++ 6.ts
@@ -0,0 +1,25 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//Addition 
+const num = 5;
+const Num = 3;
+const Result = num + Num;
+console.log(`${num} + ${Num} = ${Result}`);
+
+//Subtraction 
+const num1 = 20;
+const num2 = 12;
+const result = num1-num2;
+console.log(`${num1} - ${num2} = ${result}`);
+
+//Multiplication
+const Num1 = 2;
+const Num2 = 4;
+const RESULT = Num1*Num2;
+console.log(`${Num1} * ${Num2} = ${RESULT}`);
+
+// Division 
+const  number1 = 40;
+const  number2 = 5;
+const  REsult = number1/number2;
+console.log(`${number1} / ${number2} = ${REsult}`);
