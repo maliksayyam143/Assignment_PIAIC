c4f33adad12067b20cbacf8be07b6d4535188fa9
Sayyam_Shahid <sayyamshahid143@gmail.com>
2023-11-06 00:15:27 -0800

    9 assignment is completed

---
+++ 9.js
@@ -0,0 +1,19 @@
+// Program written by [Sayyam Shahid]
+// Date: 2023-11-06
+// This TypeScript program calculates the sum of the first 10 natural numbers.
+var sum = 0;
+for (var i_1 = 1; i_1 <= 10; i_1++) {
+    sum += i_1;
+}
+console.log("The sum of the first 10 natural numbers is: ".concat(sum));
+// Program written by [Sayyam Shahid]
+// Date: 2023-11-06
+// This TypeScript program calculates and prints the multiplication table of 15.
+var number = 15;
+var i = 1;
+console.log("Multiplication Table of ".concat(number, ":"));
+while (i <= 10) {
+    var result = number * i;
+    console.log("".concat(number, " x ").concat(i, " = ").concat(result));
+    i++;
+}
---
+++ 9.ts
@@ -0,0 +1,25 @@
+// Program written by [Sayyam Shahid]
+// Date: 2023-11-06
+// This TypeScript program calculates the sum of the first 10 natural numbers.
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+console.log(`The sum of the first 10 natural numbers is: ${sum}`);
+
+// Program written by [Sayyam Shahid]
+// Date: 2023-11-06
+// This TypeScript program calculates and prints the  table of 15.
+
+const number = 15;
+let i = 1;
+
+console.log(`Table of ${number}:`);
+while (i <= 10) {
+    const result = number * i;
+    console.log(`${number} x ${i} = ${result}`);
+    i++;
+}
